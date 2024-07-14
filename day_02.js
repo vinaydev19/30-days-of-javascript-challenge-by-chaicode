50cfc3983d5c9400a55a9a0c730530bffdd537f5
vinay1945 <vinaykumarporandla19@gmail.com>
2024-07-14 23:01:39 +0530

    day 2 complate

---
+++ day_02.js
@@ -0,0 +1,56 @@
+// Activity 1: Arithmetic Operations.
+// task 1
+let a = 3;
+let b = 4;
+console.log(a + b);
+
+// task 2
+console.log(a - b);
+
+// task 3
+console.log(a * b);
+
+// task 4
+console.log(a / b);
+
+// task 5
+console.log(a % b);
+
+// Activity 2: Assignment Operators
+// task 6
+console.log((a += 5));
+
+// task 7
+console.log((a -= 50));
+
+// Activity 3: Comparison Operators
+console.log(5 < 10);
+console.log(5 > 10);
+
+// task 9
+console.log(5 <= 5);
+console.log(5 >= 5);
+
+// task 10
+console.log(5 == "5");
+console.log(5 === "5");
+
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+let num4 = 40;
+
+// task 11
+console.log(num4 > num3 && num2 > num1);
+
+
+// task 12
+console.log(num4 > num3 || num2 < num1);
+
+
+// task 13
+console.log(!num1);
+
+// task 14
+console.log(num1 > 20 ? 'num is greater than 0' : 'num is lass than 0');
+
